6e8f6e5aa99694110873d7254d8c2323763e65d5
Jesse Lu <mr.jesselu@gmail.com>
2011-05-16 15:41:05 -0700

    Demo1 now uses current sources.

--- demo1/plot_fields.m
+++ demo1/plot_fields.m
@@ -2,11 +2,12 @@ function plot_fields(dims, varargin)
 
 N = length(varargin);
 for k = 1 : N
-    v = varargin{k}
+    v = varargin{k};
     subplot(1, N, k);
     imagesc(reshape(v{2}, dims)', max(abs(v{2}(:)) + eps) * [-1 1]);
     title(v{1});
     axis equal tight;
+    set(gca, 'Ydir', 'normal');
 end
 drawnow
 
--- demo1/resonator_demo.m
+++ demo1/resonator_demo.m
@@ -11,18 +11,27 @@ help resonator_demo
 dims = [60 40]; % Size of the grid.
 N = prod(dims);
 
-omega = 0.3; % Angular frequency of desired mode.
+omega = 0.9; % Angular frequency of desired mode.
+
 
     % 
     % Build the matrices that we will be using.
     %
 
-s = @(sx, sy) shift_grid(dims, -[sx sy]); % Allows for clean definition of curl.
-curl = [s(0,0)-s(0,-1), s(0,0)-s(-1,0)]; % Here, dH/dt = curl E.
+% Allows for a clean definition of curl.
+s = @(sx, sy) mirror_shift(dims, -[sx sy]); % Mirror boundary conditions.
 
-A = curl * curl' - omega^2 * speye(N);
+% Define the curl operators.
+Ecurl = [-(s(0,0)-s(0,-1)), s(0,0)-s(-1,0)]; % Applied to E.
+Hcurl = [s(0,1)-s(0,0); -(s(1,0)-s(0,0))]; % Applied to H. 
 
-% Test out by finding eigenmodes.
-[H, D] = eigs(A, 1, 'SM');
-E = curl' * H;
+J = zeros(2*N, 1);
+M = zeros(N, 1);
+J(round(dims(1)/2 + dims(1)*dims(2)/2)) = i;
+mu_inv = speye(N);
+A = Hcurl * Ecurl - omega^2 * speye(2*N);
+b = i * omega * J - Hcurl * mu_inv * M;
+E = A \ b;
+% [E, D] = eigs(A, 1, 'SM');
+H = Ecurl * E;
 plot_fields(dims, {'Ex', E(1:N)}, {'Ey', E(N+1:end)}, {'Hz', H});
