29841081fa22952cd2e9c7522b15c7f66e9f773a
Jesse Lu <mr.jesselu@gmail.com>
2011-05-17 05:09:28 -0700

    PML now working.

--- demo1/plot_fields.m
+++ demo1/plot_fields.m
@@ -4,7 +4,7 @@ N = length(varargin);
 for k = 1 : N
     v = varargin{k};
     subplot(1, N, k);
-    imagesc(reshape(v{2}, dims)', max(abs(v{2}(:)) + eps) * [-1 1]);
+    imagesc(real(reshape(v{2}, dims))', mean(abs(v{2}(:)) + eps) * [-1 1]);
     title(v{1});
     axis equal tight;
     set(gca, 'Ydir', 'normal');
--- demo1/resonator_demo.m
+++ demo1/resonator_demo.m
@@ -8,11 +8,12 @@ help resonator_demo
     % Some optimization parameters.
     %
 
-dims = [60 40]; % Size of the grid.
+dims = 2*[160 90]; % Size of the grid.
 N = prod(dims);
 
-omega = 0.9; % Angular frequency of desired mode.
-
+omega = 0.5; % Angular frequency of desired mode.
+sigma = 1e0/omega; % Strength of PML.
+t_pml = 10; % Thickness of PML layer.
 
     % 
     % Build the matrices that we will be using.
@@ -20,14 +21,18 @@ omega = 0.9; % Angular frequency of desired mode.
 
 % Allows for a clean definition of curl.
 s = @(sx, sy) mirror_shift(dims, -[sx sy]); % Mirror boundary conditions.
+scx = @(sx, sy) stretched_coords(dims, [1 dims(1)+0.5], [sx, sy], ...
+    'x', t_pml, sigma, 2.5);
+scy = @(sx, sy) stretched_coords(dims, [1 dims(2)+0.5], [sx, sy], ...
+    'y', t_pml, sigma, 2.5);
 
-% Define the curl operators.
-Ecurl = [-(s(0,0)-s(0,-1)), s(0,0)-s(-1,0)]; % Applied to E.
-Hcurl = [s(0,1)-s(0,0); -(s(1,0)-s(0,0))]; % Applied to H. 
+% Define the curl operators as applied to E and H, respectively.
+Ecurl = [   scy(.5,.5)*-(s(0,0)-s(0,-1)),   scx(.5,.5)*(s(0,0)-s(-1,0))];  
+Hcurl = [   scy(.5,0)*(s(0,1)-s(0,0));      scx(0,.5)*-(s(1,0)-s(0,0))]; 
 
 J = zeros(2*N, 1);
 M = zeros(N, 1);
-J(round(dims(1)/2 + dims(1)*dims(2)/2)) = i;
+J(prod(dims) + round(dims(1)/2 + dims(1)*dims(2)/2)) = i;
 mu_inv = speye(N);
 A = Hcurl * Ecurl - omega^2 * speye(2*N);
 b = i * omega * J - Hcurl * mu_inv * M;
