e6261c6264448309a9c1027b2bf95716afba99ff
Jesse Lu <mr.jesselu@gmail.com>
2011-06-15 09:20:14 -0700

    test

--- demo1.m
+++ demo1.m
@@ -1,7 +1,7 @@
-% DEMO1
+% DEMO2
 %
-% First try at field and structure improvement steps.
-help demo1
+% Update both structure and field in an unbounded way to satisfy physics.
+help demo2
 
 
     %
@@ -39,11 +39,13 @@ DIMS_ = dims;
 
 lset_grid(dims);
 phi = lset_box([0 0], [1000 10]);
-phi = lset_intersect(phi, lset_complement(lset_box([0 0], [10 1000]))); % Form cross-beam.
+% phi = lset_intersect(phi, lset_complement(lset_box([0 0], [3 1000]))); % Gap.
+phi = lset_union(phi, lset_box([0 0], [10 1000])); % Cross-beam.
 phi = lset_complement(phi);
 
 % Initialize phi, and create conversion functions.
-[phi, phi2p, phi2e, phi2eps, p2e, e2p, p2eps, eps2p, phi_smooth] = ...
+[phi, phi2p, phi2e, phi2eps, p2e, e2p, p2eps, eps2p, ...
+    A_spread, A_gather, phi_smooth] = ...
     setup_levelset(phi, eps_lo, eps_hi, 1e-3);
 
 % lset_plot(phi); pause % Use to visualize the initial structure.
@@ -53,37 +55,7 @@ phi = lset_complement(phi);
     % Find the input and output modes.
     %
 
-[Ex, Ey, Hz] = setup_border_vals({'x-', 'x+'}, omega, phi2eps(phi));
-
-
-    %
-    % Get the physics matrices and solve.
-    %
-
-% Obtain physics matrix.
-[A0, B, d] = setup_physics(omega, p2e);
-A = @(phi) A0(phi2p(phi));
-
-% Obtain the matrices for the boundary-value problem.
-[Ahat, bhat, add_border, rm_border] = ...
-    setup_border_insert(A(phi), [Ex(:); Ey(:); Hz(:)]);
-
-% Solve the boundary-value problem.
-xhat = Ahat \ -bhat;
-
-% Obtain the full field (re-insert the field values at the boundary).
-x = add_border(xhat);
-
-% Back-out field components.
-Ex = reshape(x(1:N), dims);
-Ey = reshape(x(N+1:2*N), dims);
-Hz = reshape(x(2*N+1:end), dims);
-
-% Calculate the values for the physics residual.
-phys_res = @(phi, x) norm(rm_border(A(phi)*x));
-fprintf('Physics residual: %e\n', phys_res(phi, x));
-% norm(Ahat*xhat + bhat) % Alternate definition.
-
+[Ex, Ey, Hz] = setup_border_vals({'x-', 'y-'}, omega, phi2eps(phi));
 
     %
     % Mark the region where we will allow the structure to change.
@@ -91,36 +63,51 @@ fprintf('Physics residual: %e\n', phys_res(phi, x));
 
 eta = lset_box([0 0], [40 40]);
 
-
-    % 
-    % Compute the partial derivative of the physics residual relative to p.
-    %
-
-res = [];
+p = phi2p(phi);
 for k = 1 : 100
-    dp = (eta < 0) .* e2p(B(x)' * (B(x) * phi2e(phi) - d(x)));
-    phi = update_interface(phi, [], real(dp), 0, 1e-2);
-    res(k) = phys_res(phi, x);
-    fprintf('.');
+
+        %
+        % Get the physics matrices and solve.
+        %
+
+    % Obtain physics matrix.
+    [A0, B, d] = setup_physics(omega, A_spread);
+    A = @(phi) A0(phi2p(phi));
+
+    % Obtain the matrices for the boundary-value problem.
+    [Ahat, bhat, add_border, rm_border] = ...
+        setup_border_insert(A0(p), [Ex(:); Ey(:); Hz(:)]);
+
+    % Solve the boundary-value problem.
+    xhat = Ahat \ -bhat;
+
+    % Obtain the full field (re-insert the field values at the boundary).
+    x = add_border(xhat);
+
+    % Calculate the values for the physics residual.
+    phys_res = @(p, x) norm(rm_border(A0(p)*x));
+    % norm(Ahat*xhat + bhat) % Alternate definition.
+
+    fprintf('%d: %e', k, phys_res(p, x));
+
+    p = solve_p(B(x), d(x), phi2p(phi), eta);
+    fprintf(' -> %e\n', phys_res(p, x));
+    Hz = reshape(x(2*N+1:end), dims);
+    plot_fields(dims, {'p', p}, {'|Hz|', abs(Hz)});
+    drawnow
 end
-figure(3); subplot 111; plot(res, '.-')
-min(res)/max(res)
-
-% dp = (eta < 0) .* e2p(B(x)' * (B(x) * phi2e(phi) - d(x)));
-% c = -1 : 1e-2 : 1;
-% for k = 1 : length(c) 
-%     phi1 = update_interface(phi, [], real(dp), 0, c(k));
-%     res(k) = phys_res(phi1, x);
-% end
-% figure(3); subplot 111; plot(c, res, '.-')
-
 
     %
     % Plot results.
     %
 
+% Back-out field components.
+Ex = reshape(x(1:N), dims);
+Ey = reshape(x(N+1:2*N), dims);
+Hz = reshape(x(2*N+1:end), dims);
+
 % Plot the structure.
-eps = phi2eps(phi);
+eps = p2eps(p);
 figure(1); plot_fields(dims, {'\epsilon_x', eps.x}, {'\epsilon_y', eps.y});
 
 % Plot the fields.
@@ -134,6 +121,8 @@ figure(2); plot_fields(dims, ...
 % hz = Hz(:, dims(2)/2);
 % figure(3); plot([real(ey), real(hz), real(conj(hz).*ey)]);
 % figure(3); plot_fields(dims, {'power', real(Ex.*conj(Hz))});
+figure(3); plot_fields(dims, {'p', p});
+drawnow
 
 % % Plot the residual.
 % y = real(A(phi)*x);
