6a679d01924382ba79f874f95cd1a5d9dba3ae3e
Jesse Lu <mr.jesselu@gmail.com>
2011-06-24 14:30:44 -0700

    Works with alternate step sizes for x and p.

--- cgo_opt.m
+++ cgo_opt.m
@@ -66,52 +66,64 @@ end
 
 % Initial conditions.
 fval(1) = f(var); % Starting function value.
-ss_ind(1) = 1; % Start with the largest step size.
+ss_ind{1}(1) = 1; % Start with the largest step size.
+ss_ind{2}(1) = 1; % Start with the largest step size.
 k = 1; % Index variable.
 
 % Iterate.
 for k = 1 : max_iters
 
     % Evaluate the gradient at current location.
-    grad = g(var);
+    grad_full = g(var);
 
-    % Try step sizes in descending order. 
-    % If we can decrease the objective function, then commit to taking the step.
-    while true
+    for l = 1 : 2
+        grad = grad_full;
+        switch l
+            case 1
+                grad.p = 0;
+            case 2
+                grad.x = 0;
+        end
 
-        % Compute the step and the its function value. 
-        var1 = c(var, grad, step_sizes(ss_ind(k)));
-        fval1 = f(var1);
+        % Try step sizes in descending order. 
+        % If we can decrease the objective function, 
+        % then commit to taking the step.
+        while true
 
-        % If function value decreases, take the step.
-        if (fval1 < fval(k))
-            % Update variables to prepare for next step.
-            fval(k+1) = fval1; 
-            var = var1; 
-            if (ss_ind(k) >= 2) % Increase the next iteration's step size.
-                ss_ind(k+1) = ss_ind(k) - 1;
+            % Compute the step and the its function value. 
+            var1 = c(var, grad, step_sizes(ss_ind{l}(k)));
+            fval1 = f(var1);
+
+            % If function value decreases, take the step.
+            if (fval1 < fval(k))
+                % Update variables to prepare for next step.
+                fval(k+1) = fval1; 
+                var = var1; 
+                if (ss_ind{l}(k) >= 2) % Increase the next iteration's step size.
+                    ss_ind{l}(k+1) = ss_ind{l}(k) - 1;
+                else
+                    ss_ind{l}(k+1) = ss_ind{l}(k);
+                end
+                break % Move on to next iteration.
+
+            % If function value does not decrease, take next smaller step.
             else
-                ss_ind(k+1) = ss_ind(k);
-            end
-            break % Move on to next iteration.
+                ss_ind{l}(k) = ss_ind{l}(k) + 1;
 
-        % If function value does not decrease, take next smaller step.
-        else
-            ss_ind(k) = ss_ind(k) + 1;
-
-            % If we just tried smallest step, we are done! 
-            if (ss_ind(k) > length(step_sizes))
-                ss_hist(k) = 0;
-                return
+                % If we just tried smallest step, we are done! 
+                if (ss_ind{l}(k) > length(step_sizes))
+                    ss_hist{l}(k) = 0;
+                    return
+                end
             end
         end
-    end
+        % Record the size of the step taken.
+        ss_hist{l}(k) = step_sizes(ss_ind{l}(k));
 
+    end
 %     % Done with a step, print status.
 %     fprintf('%e | %e\n', fval(k), step_sizes(ss_ind(k)));
 
-    % Record the size of the step taken.
-    ss_hist(k) = step_sizes(ss_ind(k));
 end
 
 
---
+++ demo_alternate_bounded.m
@@ -0,0 +1,123 @@
+function demo_alternate_bounded(dims, cgo_iters, savefile)
+% DEMO_JOINT_UNBOUNDED(DIMS, CGO_ITERS, IS_BOUNDED, SAVEFILE)
+%
+% Description
+%     Jointly move the field and unbounded structure variables in the direction 
+%     of steepest descent.
+% 
+% Inputs
+%     DIMS: 2-element vector of positive integers.
+%         The size of the grid over which to optimize.
+% 
+%     CGO_ITERS: Positive integer.
+%         The number of iterations to perform for the gradient-descent
+%         optimization routine.
+% 
+% Output
+%     None.
+% 
+% Examples
+%     % Quick, small examples runs.
+%     demo_indep_unbounded([30 30], 'field', 1e4);
+%     demo_indep_unbounded([30 30], 'struct', 1e4);
+
+path(path, '~/c-go'); % Make sure we have access to c-go.
+path(path, '~/level-set'); % Make sure we have access to level-set.
+
+omega = 0.15; % Angular frequency of desired mode.
+
+
+    %
+    % Helper function for determining derivative matrices.
+    % Also, helper global variables for prettier argument passing.
+    %
+
+global S_ D_ DIMS_ 
+
+% Shortcut to form a derivative matrix.
+S_ = @(sx, sy) shift_mirror(dims, -[sx sy]); % Mirror boundary conditions.
+
+% Shortcut to make a sparse diagonal matrix.
+D_ = @(x) spdiags(x(:), 0, numel(x), numel(x));
+
+DIMS_ = dims;
+N = prod(dims);
+
+
+    %
+    % Make the initial structure.
+    %
+
+lset_grid(dims);
+phi = lset_box([0 0], [1000 10]);
+eta = lset_box([0 0], dims/2);
+% phi2 = lset_union(phi, (eta)); % filled.
+phi2 = lset_intersect(phi, lset_complement(eta));
+phi = lset_complement(phi);
+eta2 = lset_box([0 0], dims/2 + 2);
+phi2 = lset_complement(phi2);
+
+% Initialize phi, and create conversion functions.
+[phi2p, phi2eps, phi_smooth] = setup_levelset(phi, 1.0, 12.25, 1e-3);
+
+
+    %
+    % Setup the constrained gradient optimization.
+    %
+
+% Objective function and its gradient.
+[f, g] = em_physics(omega); 
+
+
+% This constraint function allows both variables to change.
+bound = @(x) 1.0 * (x < 1.0) + 12.25 * (x > 12.25) + ...
+    x .* ((x >= 1.0) & (x <= 12.25));
+c = @(v, dv, s) struct( 'x', v.x - s * (field_template .* dv.x), ...
+                        'p', real(bound(v.p - s * ((eta2(:) < 0) .* dv.p))));
+
+% Initial values.
+[Ex, Ey, Hz] = setup_border_vals({'x-', 'x+'}, omega, phi2eps(phi));
+v.x = [Ex(:); Ey(:); Hz(:)];
+% randn('state', 1);
+% v.x = randn(size(v.x));
+v.p = phi2p(phi2);
+v.p = v.p(:);
+% v.p = randn(N, 1);
+
+
+    %
+    % Optimize using the c-go package.
+    %
+
+tic;
+fval = [];
+ss_hist = {[], []};
+for k = 1 : ceil(cgo_iters/1e2)
+    [v, fval0, ss_hist0] = cgo_opt(f, g, c, v, 1e2, 2.^[-20:20]); 
+    fval = [fval, fval0];
+    ss_hist{1} = [ss_hist{1}, ss_hist0{1}];
+    ss_hist{2} = [ss_hist{2}, ss_hist0{2}];
+    my_plot(v, fval, ss_hist);
+    save(savefile, 'v', 'fval', 'ss_hist');
+    fprintf('%d: %e\n', k*1e2, fval(end));
+end
+fprintf('%e, ', f(v)); toc
+
+figure(2); quick_plot(v, fval, ss_hist{1});
+figure(3); cgo_visualize(fval, ss_hist{2});
+
+function my_plot(v, fval, ss_hist)
+    
+global DIMS_
+dims = DIMS_;
+N = prod(dims);
+% figure(1); 
+plot_fields(dims, ...
+    {'Re(Ey)', real(v.x(N+1:2*N))}, {'|Ey|', abs(v.x(N+1:2*N))}, {'p', v.p});
+
+% figure(2); cgo_visualize(fval, ss_hist);
+
+drawnow
+
+
+
