00b0ce040bf3c0b9e0626cf6819db0169e297d8c
Jesse Lu <mr.jesselu@gmail.com>
2011-06-27 05:34:16 -0700

    alt lset demo not working, phi seems locked.

--- demo_alternate_levelset.m
+++ demo_alternate_levelset.m
@@ -1,25 +1,4 @@
-function demo_alternate_levelset(dims, cgo_iters, savefile)
-% DEMO_JOINT_UNBOUNDED(DIMS, CGO_ITERS, IS_BOUNDED, SAVEFILE)
-%
-% Description
-%     Jointly move the field and unbounded structure variables in the direction 
-%     of steepest descent.
-% 
-% Inputs
-%     DIMS: 2-element vector of positive integers.
-%         The size of the grid over which to optimize.
-% 
-%     CGO_ITERS: Positive integer.
-%         The number of iterations to perform for the gradient-descent
-%         optimization routine.
-% 
-% Output
-%     None.
-% 
-% Examples
-%     % Quick, small examples runs.
-%     demo_indep_unbounded([30 30], 'field', 1e4);
-%     demo_indep_unbounded([30 30], 'struct', 1e4);
+function demo_alternate_levelset(dims, cgo_iters, interval, savefile)
 
 path(path, '~/c-go'); % Make sure we have access to c-go.
 path(path, '~/level-set'); % Make sure we have access to level-set.
@@ -55,6 +34,8 @@ eta = lset_box([0 0], dims/2);
 phi2 = lset_intersect(phi, lset_complement(eta));
 phi = lset_complement(phi);
 eta2 = lset_box([0 0], dims/2 + 2);
+small_box = lset_box([0 0], dims/2 - 10);
+phi2 = lset_union(phi2, lset_intersect(small_box, lset_checkered));
 phi2 = lset_complement(phi2);
 
 % Initialize phi, and create conversion functions.
@@ -79,6 +60,7 @@ v.x = [Ex(:); Ey(:); Hz(:)];
 % randn('state', 1);
 % v.x = randn(size(v.x));
 v.phi = phi2;
+v.phi = signed_distance(v.phi, 1e-2); % Make phi more sdf-like.
 % v.p = randn(N, 1);
 
 
@@ -89,17 +71,19 @@ v.phi = phi2;
 tic;
 fval = [];
 ss_hist = {[], []};
-for k = 1 : ceil(cgo_iters/1e2)
-    [v, fval0, ss_hist0] = cgo_opt(f, g, c, v, 1e2, 2.^[-20:20]); 
+    my_plot(v, phi2p, fval, ss_hist);
+    pause
+for k = 1 : ceil(cgo_iters/interval)
+    [v, fval0, ss_hist0] = cgo_opt(f, g, c, v, interval, 2.^[-20:20]); 
     fval = [fval, fval0];
     ss_hist{1} = [ss_hist{1}, ss_hist0{1}];
     ss_hist{2} = [ss_hist{2}, ss_hist0{2}];
     my_plot(v, phi2p, fval, ss_hist);
     save(savefile, 'v', 'fval', 'ss_hist');
-    fprintf('%d: %e\n', k*1e2, fval(end));
+    fprintf('%d: %e\n', k*interval, fval(end));
 end
 fprintf('%e, ', f(v)); toc
-pause
+
 figure(2); cgo_visualize(fval, ss_hist{1});
 figure(3); cgo_visualize(fval, ss_hist{2});
 
