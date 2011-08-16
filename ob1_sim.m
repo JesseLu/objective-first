2b6250ba07286cc1ff2afe1913d26de7e4c9ca12
Jesse Lu <mr.jesselu@gmail.com>
2011-08-16 13:01:00 -0700

    Simulator working draft finished.

--- ob1_sim.m
+++ ob1_sim.m
@@ -2,12 +2,18 @@ function ob1_sim(omega, epsilon)
 
 dims = size(epsilon);
 
-[A, S] = ob1_matrices(dims, 0);
 
 % Expand epsilon.
 epsilon = cat(1, repmat(epsilon(1,:), dims(1)/2, 1), epsilon, ...
             repmat(epsilon(end,:), dims(1)/2, 1));
 epsilon = cat(2, repmat(epsilon(:,1), 1, dims(2)/2), epsilon, ...
             repmat(epsilon(:,end), 1, dims(2)/2));
+dims = size(epsilon);
+n = numel(epsilon);
 
-imagesc(epsilon')
+[A, S] = ob1_matrices(dims, 0);
+eps = A{2} * epsilon(:);
+eps = struct('x', reshape(eps(1:n), dims), 'y', reshape(eps(n+1:2*n), dims));
+
+path('~/wave-tools/em_2dte', path);
+sim_epsilon(omega, eps, '-x');
