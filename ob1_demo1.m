5b1bc23e13f38589c5cdf449e27cb05d1b41ab88
Jesse Lu <mr.jesselu@gmail.com>
2011-08-16 12:29:49 -0700

    Optimization works with bounded p (cvx).

--- ob1_demo1.m
+++ ob1_demo1.m
@@ -2,6 +2,7 @@ function ob1_demo1(dims, num_iters)
 
 N = prod(dims);
 omega = 0.15;
+path(path, genpath('~/lumos/cvx'));
 
     %
     % Form matrices.
@@ -17,9 +18,10 @@ omega = 0.15;
 % Form a simple waveguide.
 epsilon = ones(dims);
 epsilon(:,(dims(2)-10)/2:(dims(2)+10)/2) = 12.25;
-epsilon((dims(1)-10)/2:(dims(1)+10)/2, :) = 12.25;
-% epsilon(dims(1)/3:2*dims(1)/3, :) = 1.0;
+epsilon((dims(1)-10)/2:(dims(1)+10)/2,:) = 12.25;
+epsilon(dims(1)/3:2*dims(1)/3, :) = 12.25;
 
+% p0 = 1 ./ (epsilon(:) - (S.p * S.p') * (epsilon(:) - 1));
 p0 = 1 ./ epsilon(:);
 
     
@@ -47,20 +49,30 @@ phys_res = @(x, p) norm(S.r' * (A{1} * D_(A{2} * p) * A{3} * x - omega^2 * x))^2
 p = p0;
 for k = 1 : num_iters
 
-A_x = A{1} * D_(A{2} * p) * A{3} - omega^2 * speye(N); 
-x = (A_x * S.x) \ -(A_x * x0);
-x = S.x * x + x0;
+    % Solve sub-problem for field.
+    A_x = A{1} * D_(A{2} * p) * A{3} - omega^2 * speye(N); 
+    x = (A_x * S.x) \ -(A_x * x0);
+    x = S.x * x + x0;
+    fprintf('%d: %e, ', k, phys_res(x, p));
 
-A_p = A{1} * D_(A{3} * x) * A{2};
-b_p = omega^2 * x;
-fprintf('%d: %e, ', k, phys_res(x, p));
+    % Solve sub-problem for structure.
+    A_p = A{1} * D_(A{3} * x) * A{2};
+    b_p = omega^2 * x;
 
-A_hat = A_p * S.p;
-b_hat = b_p - A_p * p0;
-% p = (A_p * S.p) \ (b_p - A_p * p0);
-p = real(A_hat' * A_hat) \ real(A_hat' * b_hat);
-p = S.p * p + p0;
+    A_hat = A_p * S.p;
+    b_hat = b_p - A_p * p0;
+    % p = (A_p * S.p) \ (b_p - A_p * p0);
+    
+    cvx_quiet(true);
+    cvx_begin
+        variable p(size(A_hat,2))
+        minimize norm(A_hat * p - b_hat)
+        subject to
+            p >= 1/12.25 - S.p' * p0
+            p <= 1 - S.p' * p0
+    cvx_end
+    p = S.p * p + p0;
 
-fprintf('%e\n', phys_res(x, p));
-ob1_plot(x, p, dims, 'quick');
+    fprintf('%e\n', phys_res(x, p));
+    ob1_plot(x, p, dims, 'quick');
 end
