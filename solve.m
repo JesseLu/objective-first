b883b9fcf81f82368d935d2ee950658a59983a3a
Jesse Lu <mr.jesselu@gmail.com>
2011-10-13 14:11:30 -0700

    Solve.m works!

--- solve.m
+++ solve.m
@@ -16,9 +16,12 @@ function [eps] = solve(spec, max_iters, min_grad, varargin)
 %         Minimum value for the norm of the gradient. The optimization will
 %         stop once the norm of the gradient is below MIN_GRAD.
 % 
+%         Note that the norm of the gradient is used, and not the norm-squared.
+% 
 %     METHOD: Character string (optional).
 %         The name of the numerical method to use to solve the problem.
-%         Currently only the 'sep_convex' option is supported.
+%         Currently only the 'alt_dir' option is supported. 'alt_dir'
+%         requires the CVX package (www.stanford.edu/~boyd/cvx).
 % 
 % Outputs
 %     EPS: 2d array.
@@ -44,7 +47,7 @@ N = prod(dims);
 
 [A, S] = ob1_matrices(dims); % Relevant matrices.
 
-x0 = spec.Hz0(:); % Initial value of x (not needed for sep_convex method)
+x0 = spec.Hz0(:); % Initial value of x.
 x_int = S.int * x0; % Interior values of the field (variable).
 x_bnd = S.bnd * x0; % Boundary values of the field (fixed).
 
@@ -57,7 +60,7 @@ p_bnd = S.bnd * p0; % Boundary values of the structure (fixed).
 
 
     %
-    % Construct helper functions.
+    % Construct helper functions related to the physics residual.
     %
 
 my_diag = @(z) spdiags(z, 0, length(z), length(z)); % Make diagonal matrix.
@@ -72,9 +75,106 @@ A_p = @(x) p_scale * A{1} * my_diag(A{3} * x) * A{2};
 b_p = @(x) spec.omega^2 * x - A_p(x) * (p_offset ./ p_scale); 
 
 % Physics residual.
-phys_res = @(x, p) norm(A_x(p) * x)^2;
-phys_res_alt = @(x, p) norm(A_p(x) * p - b_p(x))^2;
+phys_res = @(x, p) norm(S.res * (A_x(p) * x))^2;
+% phys_res_alt = @(x, p) norm(S.res * (A_p(x) * p - b_p(x)))^2;
 
-phys_res(x0, p0)
-phys_res_alt(x0, p0)
-eps = nan;
+% Gradient of the physics residual. 
+grad = @(x, p) [((S.res * A_x(p))' * (S.res * A_x(p)) * x); ...
+                (S.res * A_p(x))'*(S.res * (A_p(x) * p - b_p(x)))];
+                
+
+    %
+    % Helper functions for optimization process.
+    %
+
+% Functions to include add boundary values to interior values of x and p.
+p_full = @(pin) S.int' * pin + S.bnd' * p_bnd;
+x_full = @(xin) S.int' * xin + S.bnd' * x_bnd;
+
+% Progress functions.
+progress = @(xin, pin) [phys_res(x_full(xin), p_full(pin)), ...
+                        norm(grad(x_full(xin), p_full(pin)))];
+print_prog = @(iter, xin, pin) ...
+    fprintf('%5d: \t%1.5e \t%1.5e\n', iter, progress(xin, pin));
+print_header = @(str) ...
+    fprintf('%s iter#: \t[phys_res] \t[grad_norm]\n', str);
+
+
+    %
+    % Perform the optimization.
+    %
+
+% Resolve what optimization method we should use.
+if isempty(varargin)
+    method = 'alt_dir';
+else
+    method = varargin{1};
+end
+
+switch method
+    case 'alt_dir'
+        % Alternating directions method.
+        % *   The basic idea is to alternately optimize for x and then p.
+        % *   This allows us to use standard, guaranteed-to-work solvers.
+        % *   Although this method is slow, it should always approach a 
+        %     solution.
+
+        fprintf('Starting the alternating directions solver...\n');
+        print_header('x/p'); % Print header information for progress.
+        start_time = tic; % For timing purposes.
+
+        for k = 1 : max_iters
+            % Solve for x_int.
+            x_int = (A_x(p_full(p_int)) * S.int') \ ...
+                    (-A_x(p_full(p_int)) * S.bnd' * x_bnd);
+            fprintf('(x) '); print_prog(k, x_int, p_int)
+
+            % Solve for p_int.
+            cvx_quiet(true);
+            cvx_begin
+                variable p_int(length(p_int))
+                minimize norm(A_p(x_full(x_int)) * ...
+                                (S.int' * p_int + S.bnd' * p_bnd) - ...
+                                b_p(x_full(x_int)))
+                subject to
+                    p_int >= 0
+                    p_int <= 1
+            cvx_end
+            fprintf('(p) '); print_prog(k, x_int, p_int)
+
+            % Visualize.
+            figure(1);
+            ob1_plot(dims,  {'p', p_full(p_int)}, ...
+                            {'|x|', abs(x_full(x_int))}, ...
+                            {'Re(x)', real(x_full(x_int))});
+
+            % Check for gradient norm stopping condition.
+            if (norm(grad(x_full(x_int), p_full(p_int))) < min_grad)
+                fprintf('Gradient norm stopping condition satisfied.');
+                break
+            end
+        end
+
+
+
+    otherwise
+        error('Invalid choice of METHOD (%s).', method);
+end
+
+    
+    %
+    % Extract the full design and print final result.
+    %
+
+eps = reshape(p2ie(p_full(p_int)).^-1, dims); % Get full structure.
+
+fprintf('\n%d iterations completed in %1.1f seconds.\n', k, toc(start_time));
+fprintf('Final physics residual: %1.5e\nFinal gradient norm: %1.5e\n', ...
+    progress(x_int, p_int));
+
+% Visualize.
+figure(1);
+ob1_plot(dims,  {'p', p_full(p_int)}, ...
+                {'|x|', abs(x_full(x_int))}, ...
+                {'Re(x)', real(x_full(x_int))});
+
