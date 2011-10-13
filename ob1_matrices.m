d2e78fc25926a44590ae63ff50f7a12c1d57d2d0
Jesse Lu <mr.jesselu@gmail.com>
2011-10-13 11:26:36 -0700

    Physics residual seems to work for solve.m.

--- ob1_matrices.m
+++ ob1_matrices.m
@@ -1,11 +1,12 @@
-function [A, S] = ob1_matrices(dims, active_box)
+function [A, S] = ob1_matrices(dims)
+% Form matrices used for objective-first method.
 
     %
     % Form A matrices.
     %
 
 % Shortcut to form a 2D derivative matrix.
-S_ = @(sx, sy) shift_mirror(dims, -[sx sy]); % Mirror boundary conditions.
+S_ = @(sx, sy) ob1_shift_matrix(dims, -[sx sy]); % Mirror boundary conditions.
 
 A{1} = [-(S_(0,1)-S_(0,0)),  (S_(1,0)-S_(0,0))]; % Curl for E-field.
 A{2} = 0.5 * [S_(0,0)+S_(1,0); S_(0,0)+S_(0,1)]; % Spread for epsilon.
@@ -13,17 +14,30 @@ A{3} = [(S_(0,0)-S_(0,-1)); -(S_(0,0)-S_(-1,0))]; % Curl for H-field.
 
     
     %
-    % Form selection matrices, used to select active elements of x and p.
+    % Form selection matrices, used to select either interior,
+    % or boundary elements of x and p.
     %
 
-S.x = my_selection(dims, [2 2]);
-S.p = my_selection(dims, ceil((dims - active_box) / 2));
-S.r = my_selection(dims, [1 1]);
+[S.int, S.bnd] = my_selection(dims, [2 2]); % 2-cell border on all four sides.
+[S.res] = my_selection(dims, [1 1]); % Used to calculate the residual.
 
 
-function [S] = my_selection(dims, border)
-S = zeros(dims);
-S(border(1)+1:end-border(1), border(2)+1:end-border(2)) = 1;
-ind = find(S);
-n = length(ind);
-S =  sparse(ind, 1:n, ones(n,1), prod(dims), n);
+
+function [int, bnd] = my_selection(dims, border)
+% Form selection matrices for both interior and boundary values.
+
+% Temporary field which we use to select boundary elements.
+field = zeros(dims);
+field(border(1)+1:end-border(1), border(2)+1:end-border(2)) = 1;
+
+% Find which elements are either on the interior or on the boundary.
+ind_int = find(field == 1);
+ind_bnd = find(field == 0);
+
+% Function which forms the sparse matrix.
+my_sparse = @(ind) sparse(1:length(ind), ind, ones(length(ind),1), ...
+                            length(ind), prod(dims));
+
+% For the selection matrices.
+int = my_sparse(ind_int);
+bnd = my_sparse(ind_bnd);
--- solve.m
+++ solve.m
@@ -16,7 +16,7 @@ function [eps] = solve(spec, max_iters, min_grad, varargin)
 %         Minimum value for the norm of the gradient. The optimization will
 %         stop once the norm of the gradient is below MIN_GRAD.
 % 
-%     METHOD: Character string.
+%     METHOD: Character string (optional).
 %         The name of the numerical method to use to solve the problem.
 %         Currently only the 'sep_convex' option is supported.
 % 
@@ -24,4 +24,57 @@ function [eps] = solve(spec, max_iters, min_grad, varargin)
 %     EPS: 2d array.
 %         The permittivity values of the final design.
     
+% Implementation notes
+% *   We attempt to completely use linear-algebra language in this function,
+%     which means everything is either a matrix or a vector (no 2-d arrays
+%     for example). The implementation is much clearer this way.
+% *   We fix the range of p to be from 0 to 1 (inclusive). In order to match
+%     the desired range in epsilon values, p is scaled and offset.
+% *   For numerical reasons (convexity in the p variable) we use the inverse
+%     of epsilon instead of epsilon itself.
 
+
+dims = size(spec.eps0);
+N = prod(dims);
+
+
+    % 
+    % Form relevant matrices, and get initial values of x and p.
+    %
+
+[A, S] = ob1_matrices(dims); % Relevant matrices.
+
+x0 = spec.Hz0(:); % Initial value of x (not needed for sep_convex method)
+x_int = S.int * x0; % Interior values of the field (variable).
+x_bnd = S.bnd * x0; % Boundary values of the field (fixed).
+
+p_scale = diff(spec.eps_lims.^-1); % Scaling factor for p.
+p_offset = (spec.eps_lims(1).^-1) * ones(N,1); % Offset for values of p.
+
+p0 = p_scale^-1 * (spec.eps0(:).^-1 - p_offset); % Initial value of p.
+p_int = S.int * p0; % Interior values of the structure (variable).
+p_bnd = S.bnd * p0; % Boundary values of the structure (fixed).
+
+
+    %
+    % Construct helper functions.
+    %
+
+my_diag = @(z) spdiags(z, 0, length(z), length(z)); % Make diagonal matrix.
+
+p2ie = @(p) p_scale * p + p_offset; % Transform from p to 1/eps.
+
+% Physics operator in terms of x.
+A_x = @(p) A{1} * my_diag(A{2} * p2ie(p)) * A{3} - spec.omega^2 * speye(N);
+
+% Physics operator in terms of p (A_p * p - b_p = 0).
+A_p = @(x) p_scale * A{1} * my_diag(A{3} * x) * A{2};
+b_p = @(x) spec.omega^2 * x - A_p(x) * (p_offset ./ p_scale); 
+
+% Physics residual.
+phys_res = @(x, p) norm(A_x(p) * x)^2;
+phys_res_alt = @(x, p) norm(A_p(x) * p - b_p(x))^2;
+
+phys_res(x0, p0)
+phys_res_alt(x0, p0)
+eps = nan;
