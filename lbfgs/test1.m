fcc235f4f020ebd48340b7d236f1067171bb18c7
Jesse Lu <mr.jesselu@gmail.com>
2011-11-02 17:08:54 -0700

    Added specific test for lbfgs and cleaned up a bit.

--- lbfgs/test1.m
+++ lbfgs/test1.m
@@ -31,30 +31,8 @@ end
 tic
 % Custom, simple lbfgs algorithm, using lbfgs_update.
 fprintf('\nCustom lbfgs algorithm\n');
-n_max = 5;
-x = zeros(n, 1);
-[f, g] = feval(fun, x);
-h = [];
-num_iters = 0;
-while norm(g)/n > tol % Termination condition.
-    if isempty(h) % Initialize.
-        [delta, M, W, h] = lbfgs_update(x, g, n_max, h);
-        p = -g; % Just go in steepest-descent direction.
-    else
-        [delta, M, W, h] = lbfgs_update(x, g, n_max, h);
-        p = arrow_solve((delta)*ones(n,1), ones(0,n), -W*M, W, -g);
-    end
-    
-    f_t = @(t) fun(x + t * p);
-    t = backtrack_linesearch(f_t, 1, f_t(0), (g' * p), 0.1, 0.5);
-
-    x_prev = x;
-    g_prev = g;
-
-    x = x_prev + t * p;
-    [f, g] = fun(x);
-    num_iters = num_iters + 1;
-end
+[x, num_iters] = lbfgs(fun, zeros(n, 1), tol);
+[f, g] = fun(x);
 fprintf('%d iterations, fval = %e, ||g||/n = %e\n', num_iters, f, norm(g)/n);
 
 
---
+++ lbfgs/test_lbfgs.m
@@ -0,0 +1,22 @@
+function [] = test1(n, tol, max_iters)
+% Test lbfgs against a simple unconstrained quadratic problem.
+
+randn('state', 1);
+A = spdiags(randn(n, 7), -3:3, n, n);
+b = randn(n, 1);
+x_star = A \ b;
+
+% Solve using lbfgs.
+fun = @(x) my_quad(A, b, x);
+
+tic
+% Custom, simple lbfgs algorithm, using lbfgs_update.
+fprintf('\nCustom lbfgs algorithm\n');
+[x, num_iters] = lbfgs(fun, zeros(n, 1), tol);
+[f, g] = fun(x);
+fprintf('%d iterations, fval = %e, ||g||/n = %e\n', num_iters, f, norm(g)/n);
+
+
+function [fval, grad] = my_quad(A, b, x)
+fval = 0.5 * norm(A * x - b)^2;
+grad = A' * (A * x - b);
